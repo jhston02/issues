d3aef694f0227c4da850ecfc8afec634575880e9
Coleslaw <coleslaw.john@gmail.com>
2021-11-20 12:29:13 -0500

    logging

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule Issues.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.8.0"},
-      {:poison, "~> 5.0"}
+      {:poison, "~> 5.0"},
+      {:ex_doc, "~> 0.25.5"},
+      {:earmark, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
